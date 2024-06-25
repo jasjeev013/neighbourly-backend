cf976ef958948cbc7fb3717a3aa395e743de07ce
Jasjeev <jasjeev99@gmail.com>
2024-06-25 15:46:01 +0530

    Added variables to .env

--- config/cloudinary.js
+++ config/cloudinary.js
@@ -1,9 +1,9 @@
 const cloudinary = require('cloudinary').v2;
 
 cloudinary.config({
-  cloud_name: 'dbo1goy2k',
-  api_key: '196314761784852',
-  api_secret: 'h3Pwr_m_9vIH2nKgMMn3atnnFww'
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET
 });
 
 module.exports = cloudinary;
